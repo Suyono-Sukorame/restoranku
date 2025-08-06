13967e3a360bb154ac05c630de07fcefb784d039
Suyono Sukorame <suyonosukorame@ymail.com>
2025-08-06 21:41:23 +0800

    Enhance model relationships and attributes across Category, Item, Order, OrderItem, Role, and User models

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends Model
 {
-    //
+    use SoftDeletes, HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category_id',
+        'img',
+        'is_active',
+        'create_at',
+        'updated_at',
+    ];
+    protected $dates = ['delete_at'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OrderItem extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'order_id',
+        'item_id',
+        'quantity',
+        'price',
+        'tax',
+        'total_price',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    // Relasi ke Order
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    // Relasi ke Item
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
 }
