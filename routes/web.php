de07363c3fef9c930786044beba90c6cc4879d01
Suyono Sukorame <suyonosukorame@ymail.com>
2025-08-12 22:33:38 +0800

    Sidebar & Routes Admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\MenuController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
@@ -17,5 +21,21 @@ Route::get('/checkout', [MenuController::class, 'checkout'])->name('checkout');
 Route::post('/checkout/store', [MenuController::class, 'storeOrder'])->name('checkout.store');
 Route::get('checkout/success/{orderId}', [MenuController::class, 'checkoutSuccess'])->name('checkout.success');
 
+Route::get('/dashboard', function () {
+    return view('admin.dashboard'); // sesuaikan dengan view dashboard kamu
+})->name('dashboard');
 
-Route::resource('categories', CategoryController::class)->names('admin.categories');
+Route::resource('orders', OrderController::class)->names('orders');
+Route::resource('items', ItemController::class)->names('items');
+Route::resource('users', UserController::class)->names('users');
+Route::resource('roles', RoleController::class)->names('roles');
+Route::resource('categories', CategoryController::class)->names('categories');
+
+
+// Route::resource('categories', CategoryController::class)->names('admin.categories');
+// Route::resource('items', CategoryController::class)->names('admin.items');
+// Route::resource('roles', CategoryController::class)->names('admin.roles');
+// Route::resource('users', CategoryController::class)->names('admin.users');
+// Route::resource('orders', CategoryController::class)->names('admin.orders');
+
+
